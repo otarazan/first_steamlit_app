5099d08c0e553c35596fbbd0da5a1aa97856fb4c
olcay tarazan <olcaytarazan@gmail.com>
2022-12-30 20:43:42 -0600

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -1,6 +1,10 @@
 import streamlit as streamlit
 import pandas
 import numpy as np
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
