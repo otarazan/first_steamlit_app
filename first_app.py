0c152f81ea5d1d02709c094abe0739c148c61dae
olcay tarazan <olcaytarazan@gmail.com>
2022-12-31 01:12:12 -0600

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -1,6 +1,7 @@
 import streamlit as streamlit
 import pandas
 import numpy as np
+import snowflake.connector
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
