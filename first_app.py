b4c16edf2711e927ef377773a08c2ca21a51337c
olcay tarazan <olcaytarazan@gmail.com>
2022-12-30 21:12:45 -0600

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -3,7 +3,8 @@ import pandas
 import numpy as np
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
